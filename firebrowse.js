251b970a5ada16da34f723676daa3454509deadb
Jonas Almeida <jalmeida@mathbiol.org>
2019-04-24 10:14:47 -0400

    get method

--- firebrowse.js
+++ firebrowse.js
@@ -1,1 +1,18 @@
 console.log('firebrowse.js loaded')
+
+firebrowse={}
+
+firebrowse.get=async function(url){
+    return (await fetch(url)).json()
+}
+
+firebrowse.hello='hello at '+Date()
+
+if(typeof(define)!='undefined'){
+    define(firebrowse)
+}
+
+/*
+url = 'http://firebrowse.org/api/v1/Samples/mRNASeq?format=json&gene=PSCA&cohort=BLCA&protocol=RSEM&page=1&page_size=250&sort_by=cohort'
+x = firebrowse.get(url)
+*/
